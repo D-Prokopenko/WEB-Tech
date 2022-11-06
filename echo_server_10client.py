ca89e48af6855941421c2631de3c66c3c403a1cc
D-Prokopenko <denis1489@list.ru>
2022-11-06 14:20:04 +0300

    лишний цикл

--- echo_server_10client.py
+++ echo_server_10client.py
@@ -6,9 +6,8 @@ serv.listen(10)  # количество подключений (клиентов
 
 while True:
     conn, adrr = serv.accept()
-    while True:
-        data = conn.recv(1024)
-        if not data or data == 'close':
-            break
-        conn.send(data)
+    data = conn.recv(1024)
+    if not data or data == 'close':
+        break
+    conn.send(data)
     conn.close()
